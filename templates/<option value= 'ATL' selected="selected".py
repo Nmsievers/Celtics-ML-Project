5c5f5765829eba5755fe086225e48caf7e115c32
Nate Sievers <67162537+Nmsievers@users.noreply.github.com>
2022-04-14 14:32:38 -0400

    Add files via upload

---
+++ templates/<option value= 'ATL' selected="selected".py
@@ -0,0 +1,29 @@
+                        <option value= 'ATL' selected="selected">Atlanta Hawks</option>
+                        <option value= 'BNK' selected="selected">Brooklyn Nets</option>
+                        <option value= 'CHI' selected="selected">Chicago Bulls</option>
+                        <option value= 'CHO' selected="selected">Charlotte Hornets</option>
+                        <option value= 'CLE' selected="selected">Cleveland Cavaliers</option>
+                        <option value= 'DAL' selected="selected">Dallas Mavericks</option>
+                        <option value= 'DEN' selected="selected">Denver Nuggets</option>
+                        <option value= 'DET' selected="selected">Detroit Pistons</option>
+                        <option value= 'GSW' selected="selected">Golden State Warriors</option>
+                        <option value= 'HOU' selected="selected">Houston Rockets</option>
+                        <option value= 'IND' selected="selected">Indiana Pacers</option>
+                        <option value= 'LAC' selected="selected">Los Angeles Clippers</option>
+                        <option value= 'LAL' selected="selected">Los Angeles Lakers</option>
+                        <option value= 'MEM' selected="selected"></option>Memphis Grizzlies</option>
+                        <option value= 'MIA' selected="selected">Miami Heat</option>
+                        <option value= 'MIL' selected="selected">Milwaukee Bucks</option>
+                        <option value= 'MIN' selected="selected">Minnesota Timberwolves</option>
+                        <option value= 'NOP' selected="selected">New Orleans Pelicans</option>
+                        <option value= 'NYK' selected="selected">New York Knicks</option>
+                        <option value= 'OKC' selected="selected">Oklahoma City Thunder</option>
+                        <option value= 'ORL' selected="selected">Orlando Magic</option>
+                        <option value= 'PHI' selected="selected">Philadelphia 76ers</option>
+                        <option value= 'PHO' selected="selected">Phoenix Suns</option>
+                        <option value= 'POR' selected="selected">Portland Trail Blazers</option>
+                        <option value= 'SAC' selected="selected">Sacremento Kings</option>
+                        <option value= 'SAS' selected="selected">San Antonio Spurs</option>
+                        <option value= 'TOR' selected="selected">Toronto Raptors</option>
+                        <option value= 'UTA' selected="selected">Utah Jazz</option>
+                        <option value= 'WAS' selected="selected">Washington Wizards</option>
